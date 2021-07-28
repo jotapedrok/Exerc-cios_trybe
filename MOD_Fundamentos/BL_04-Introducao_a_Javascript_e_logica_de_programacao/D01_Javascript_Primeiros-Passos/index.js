d822945df9e03fba2e46c8e5d7fd23fd75d23471
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-07-28 18:59:00 -0300

    exercicio 1

---
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D01_Javascript_Primeiros-Passos/index.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 5;
+
+console.log("Soma: " + (a+b));
+
+console.log("Subtração: " + (a-b));
+
+console.log("Mulriplicação: " + (a*b));
+
+console.log("Divisão: " + (a/b));
+
+console.log("Módulo: " + (a%b));
