72baded8c8ae54d653af954b210e37cf5612f62a
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-07-28 19:04:02 -0300

    exercicio 2

--- MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D01_Javascript_Primeiros-Passos/index.js
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D01_Javascript_Primeiros-Passos/index.js
@@ -1,12 +1,19 @@
 let a = 10;
 let b = 5;
 
-console.log("Soma: " + (a+b));
+// console.log("Soma: " + (a+b));
 
-console.log("Subtração: " + (a-b));
+// console.log("Subtração: " + (a-b));
 
-console.log("Mulriplicação: " + (a*b));
+// console.log("Mulriplicação: " + (a*b));
 
-console.log("Divisão: " + (a/b));
+// console.log("Divisão: " + (a/b));
 
-console.log("Módulo: " + (a%b));
+// console.log("Módulo: " + (a%b));
+
+if (a>b) {
+    console.log(a +" é maior que "+ b)
+}
+else {
+    console.log(b +" é maior que "+ a)
+}
