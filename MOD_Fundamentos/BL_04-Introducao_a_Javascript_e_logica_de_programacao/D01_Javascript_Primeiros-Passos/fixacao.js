a98d91498b5283cf2af27ce9113a085a6d06ba82
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-07-28 18:17:23 -0300

    fixacao 2

--- MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D01_Javascript_Primeiros-Passos/fixacao.js
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D01_Javascript_Primeiros-Passos/fixacao.js
@@ -1,15 +1,25 @@
-const myName = "João Pedro";
-const birthCity = "Niterói";
-var birthYear = 1999
+// const myName = "João Pedro";
+// const birthCity = "Niterói";
+// var birthYear = 1999
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
 
-birthYear = 2030
+// birthYear = 2030
 
-console.log(birthYear);
+// console.log(birthYear);
 
-birthCity = "otro lugar";
+// birthCity = "otro lugar";
 
-console.log(birthCity);
+// console.log(birthCity);
+let base = 8;
+let altura = 5;
+let area = base*altura;
+let perimetro = base*2 + altura*2;
+
+console.log(area);
+console.log(base*altura)
+
+console.log(perimetro)
+console.log(base*2 + altura*2)
