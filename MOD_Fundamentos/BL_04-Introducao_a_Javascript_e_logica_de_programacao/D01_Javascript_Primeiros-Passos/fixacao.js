daa10cba076356b50355f395766c6fad9f8a92bd
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-07-28 16:02:56 -0300

    fixacao 1

---
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D01_Javascript_Primeiros-Passos/fixacao.js
@@ -0,0 +1,15 @@
+const myName = "João Pedro";
+const birthCity = "Niterói";
+var birthYear = 1999
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030
+
+console.log(birthYear);
+
+birthCity = "otro lugar";
+
+console.log(birthCity);
