82aa89d8dae25966123b2b1ff2529ec62a25c6d3
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-07-30 18:20:58 -0300

    exercicio 1

---
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D03_Javascript_Logica-de-programaçao-e-algoritimos/exercicio1.js
@@ -0,0 +1,11 @@
+let n = 2;
+let asteriscos = [];
+let linhaVezes = [];
+
+for (let index = 1; index <= n; index += 1) {
+    asteriscos.push('*')
+}
+for (let vezes = 1; vezes <= n; vezes += 1){
+console.log(asteriscos);
+}
+
