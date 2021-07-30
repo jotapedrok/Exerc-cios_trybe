6f0b9b1af6adb3b58dffbeb9b81c952ca581b6e9
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-07-30 18:42:12 -0300

    corrige exercicio 2

--- MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D03_Javascript_Logica-de-programaçao-e-algoritimos/exercicio2.js
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D03_Javascript_Logica-de-programaçao-e-algoritimos/exercicio2.js
@@ -1,9 +1,9 @@
 let n = 5;
-let asteriscos = [];
-let linhaVezes = [];
+let asteriscos = "*";
+let linhaVezes = "";
 
-for (let index = 1; index <= n; index += 1) {
-    asteriscos.push('*')
-    console.log (asteriscos)
+for (let index = 0; index <= n; index += 1) {
+    console.log (linhaVezes)
+    linhaVezes = linhaVezes + asteriscos
 }
 
