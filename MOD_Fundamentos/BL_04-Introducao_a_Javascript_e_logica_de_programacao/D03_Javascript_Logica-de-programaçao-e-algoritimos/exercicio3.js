4f472c9f6d510a35b7b9ff2aaace80e42d14774a
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-07-30 18:57:30 -0300

    exercicio 3

--- MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D03_Javascript_Logica-de-programaçao-e-algoritimos/exercicio3.js
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D03_Javascript_Logica-de-programaçao-e-algoritimos/exercicio3.js
@@ -1,9 +1,19 @@
 let n = 5;
-let asteriscos = [];
-let linhaVezes = [];
+let asteriscos = "*";
+let linhaVezes = "";
+let posiçao = n
 
-for (let index = 1; index <= n; index += 1) {
-   
-    console.log (asteriscos)
+for (let index = 0; index <= n; index += 1) {
+    for (positindex = 0; positindex <= n; positindex += 1){
+        if (positindex < posiçao) {
+            linhaVezes = linhaVezes + ' '
+        }
+        else {
+            linhaVezes = linhaVezes + asteriscos
+        }
+    }
+    console.log (linhaVezes)
+    linhaVezes = "";
+    posiçao -= 1
+
 }
-
