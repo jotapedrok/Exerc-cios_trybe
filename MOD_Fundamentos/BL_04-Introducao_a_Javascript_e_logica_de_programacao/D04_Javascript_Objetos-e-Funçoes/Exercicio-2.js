efedabec14407db3aa6d60d2ccd3008898006490
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-02 20:24:44 -0300

    exercicio2.4

--- MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D04_Javascript_Objetos-e-Funçoes/Exercicio-2.js
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D04_Javascript_Objetos-e-Funçoes/Exercicio-2.js
@@ -12,14 +12,26 @@
 // palindromo('desenvolvimento')
 // palindromo('arara')
 
-function maiorValorindex (numeros) {
-    let maiorIndex = 0;
+// function maiorValorindex (numeros) {
+//     let maiorIndex = 0;
+//     for (let key in numeros) {
+//         if (numeros[maiorIndex] < numeros[key]) {
+//             maiorIndex = key
+//         }
+//     }
+//     console.log(maiorIndex)
+// }
+
+// maiorValorindex ([2, 4, 6, 7, 10, 0, -3]);
+
+function menorValorindex (numeros) {
+    let menorIndex = 0;
     for (let key in numeros) {
-        if (numeros[maiorIndex] < numeros[key]) {
-            maiorIndex = key
+        if (numeros[menorIndex] > numeros[key]) {
+            menorIndex = key
         }
     }
-    console.log(maiorIndex)
+    console.log(menorIndex)
 }
 
-maiorValorindex ([2, 4, 6, 7, 10, 0, -3]);
+menorValorindex ([2, 4, 6, 7, 10, 0, -3]);
