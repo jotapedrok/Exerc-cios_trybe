6d8a136ad03f078c14a8bcf31b066a6dfb96a51f
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-02 18:59:57 -0300

    exercicio1.4

--- MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D04_Javascript_Objetos-e-Funçoes/Exercicio-1.js
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D04_Javascript_Objetos-e-Funçoes/Exercicio-1.js
@@ -10,6 +10,27 @@ info['recorrente'] = 'Sim'
 
 //  console.log(info)
 
-for(let key in info) {
-    console.log(key)
-}
+//  for(let key in info) {
+//     console.log(key)
+// }
+
+// for(let key in info) {
+//     console.log(info[key])
+// }
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  };
+
+
+  for(let key in info) {
+      if (info[key] === info2[key]) {
+        console.log('Ambos Recorrentes')
+      } 
+      else {
+        console.log(info[key] + ' e ' + info2[key])
+      }
+    }
