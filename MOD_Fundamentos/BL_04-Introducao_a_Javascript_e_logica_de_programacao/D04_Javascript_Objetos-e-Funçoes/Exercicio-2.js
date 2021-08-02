5145b84916e00fb2574c506e252d35b5221a48de
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-02 20:35:33 -0300

    exercicio2.5

--- MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D04_Javascript_Objetos-e-Funçoes/Exercicio-2.js
+++ MOD_Fundamentos/BL_04-Introducao_a_Javascript_e_logica_de_programacao/D04_Javascript_Objetos-e-Funçoes/Exercicio-2.js
@@ -24,14 +24,27 @@
 
 // maiorValorindex ([2, 4, 6, 7, 10, 0, -3]);
 
-function menorValorindex (numeros) {
-    let menorIndex = 0;
-    for (let key in numeros) {
-        if (numeros[menorIndex] > numeros[key]) {
-            menorIndex = key
-        }
+// function menorValorindex (numeros) {
+//     let menorIndex = 0;
+//     for (let key in numeros) {
+//         if (numeros[menorIndex] > numeros[key]) {
+//             menorIndex = key
+//         }
+//     }
+//     console.log(menorIndex)
+// }
+
+// menorValorindex ([2, 4, 6, 7, 10, 0, -3]);
+
+
+function maisLetras (nomes) {
+let letras = 0
+for (let key in nomes){
+    if (nomes[letras].length < nomes[key].length) {
+        letras = key
     }
-    console.log(menorIndex)
+}
+console.log(nomes[letras])
 }
 
-menorValorindex ([2, 4, 6, 7, 10, 0, -3]);
+maisLetras (['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])
