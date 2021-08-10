af93992072e960c63773ee5edb91fa143d592c5e
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-10 16:47:10 -0300

    ajusta cabeçalho das questoes

--- MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D02_Javascript_Trabalhando-com-elementos/Fixaçao/script.js
+++ MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D02_Javascript_Trabalhando-com-elementos/Fixaçao/script.js
@@ -1,42 +1,42 @@
-// Fixação 1
+                        // Fixação 1
 let elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
 
-// Fixação 2
+                        // Fixação 2
 let pai = elementoOndeVoceEsta.parentNode;
 pai.style.backgroundColor = 'blue';
 
-// Fixação 3
+                        // Fixação 3
 elementoOndeVoceEsta.firstElementChild.innerText = 'Lo-Fi is Amazing!';
 
-// Fixação 4
+                        // Fixação 4
 pai.firstElementChild;
 
-// Fixação 5
+                        // Fixação 5
 elementoOndeVoceEsta.previousElementSibling;
 
-// Fixação 6
+                        // Fixação 6
 elementoOndeVoceEsta.nextSibling;
 
-// Fixação 7
+                        // Fixação 7
 elementoOndeVoceEsta.nextElementSibling;
 
-// Fixação 8
+                        // Fixação 8
 pai.children[2];
 
-// Fixação 9
+                        // Fixação 9
 let irmaoDeOndeEuTo = document.createElement('section');
 document.querySelector('#pai').appendChild(irmaoDeOndeEuTo);
 
-// Fixação 10
+                        // Fixação 10
 let filhoDeOndeEuTo = document.createElement('section');
 document.querySelector('#elementoOndeVoceEsta').appendChild(filhoDeOndeEuTo);
 
-// Fixação 11
+                        // Fixação 11
 let filhoDoFilho = document.createElement('section');
 document.querySelector('#primeiroFilhoDoFilho').appendChild(filhoDoFilho);
 let filhoDoFilhoId = document.querySelector('#primeiroFilhoDoFilho section');
 filhoDoFilhoId.id = 'FilhoDoPrimeiroFilhoDoFilho'
 
-// Fixação 12
+                        // Fixação 12
 let filhoDoFilhoAqui = document.querySelector('#primeiroFilhoDoFilho').children[0];
 filhoDoFilhoAqui.parentElement.parentElement.nextElementSibling;
