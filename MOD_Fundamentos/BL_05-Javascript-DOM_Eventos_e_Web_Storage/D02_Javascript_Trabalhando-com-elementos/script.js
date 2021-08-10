f01678488678d0cb5028a488fccb36f046cb1a2c
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-10 18:55:53 -0300

    exercicio 1

---
+++ MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D02_Javascript_Trabalhando-com-elementos/script.js
@@ -0,0 +1,9 @@
+                    // Exercício 1
+let body = document.getElementsByTagName('body')[0];
+let title = document.createElement('h1');
+title.innerText= 'Exercício 5.2 - JavaScript DOM';
+body.appendChild(title);
+let titleId = document.querySelector('h1');
+titleId.id = 'titulo';
+
+                    // Exercício 2
