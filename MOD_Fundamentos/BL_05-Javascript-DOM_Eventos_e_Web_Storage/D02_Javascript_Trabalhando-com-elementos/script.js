82ecc32bdb3079600319b899a4fadc6f91e4d53a
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-10 18:58:35 -0300

    exercicio 2

--- MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D02_Javascript_Trabalhando-com-elementos/script.js
+++ MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D02_Javascript_Trabalhando-com-elementos/script.js
@@ -7,3 +7,8 @@ let titleId = document.querySelector('h1');
 titleId.id = 'titulo';
 
                     // Exercício 2
+let main = document.createElement('main');
+body.appendChild(main);
+let mainId = document.querySelector('main');
+mainId.id = 'main-content';
+
