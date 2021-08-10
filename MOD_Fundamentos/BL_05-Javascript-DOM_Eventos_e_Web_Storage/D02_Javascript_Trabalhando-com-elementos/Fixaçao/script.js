21d71071ffc4c295ca9c26ec17d5e8c6e5d61286
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-10 15:34:36 -0300

    fixacao 1 - 3

---
+++ MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D02_Javascript_Trabalhando-com-elementos/Fixaçao/script.js
@@ -0,0 +1,10 @@
+// Fixação 1
+let elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+
+// Fixação 2
+let pai = elementoOndeVoceEsta.parentNode;
+pai.style.backgroundColor = 'blue';
+
+// Fixação 3
+elementoOndeVoceEsta.firstElementChild.innerText = 'Lo-Fi is Amazing!';
+
