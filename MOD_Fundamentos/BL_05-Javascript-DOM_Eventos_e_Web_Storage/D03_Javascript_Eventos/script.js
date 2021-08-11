6410a245a9485ba5f91cae9e55384cecae0349dc
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-11 19:36:55 -0300

    exercicio 1

--- MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D03_Javascript_Eventos/script.js
+++ MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D03_Javascript_Eventos/script.js
@@ -16,3 +16,36 @@ function createDaysOfTheWeek() {
   // Escreva seu código abaixo.
 
                     // Exercício 1
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function diasDoMês() {
+
+    const listDays = document.querySelector('#days')
+
+for (let i = 0; i < dezDaysList.length; i += 1) {
+    let day = dezDaysList[i]
+    let dayLi = document.createElement('li');
+    dayLi.className = 'day'
+
+    if (day === 24 || day === 31) {
+        dayLi.classList.add('holiday');
+        dayLi.innerHTML = day;
+        listDays.appendChild(dayLi);
+    } else if (day === 4 || day === 11 || day === 18) {
+        dayLi.classList.add('friday');
+        dayLi.innerHTML = day;
+        listDays.appendChild(dayLi); 
+    } else if ( day === 25) {
+        dayLi.classList.add('friday', 'holiday');
+        dayLi.innerHTML = day;
+        listDays.appendChild(dayLi); 
+    } else {
+        dayLi.innerHTML = day;
+        listDays.appendChild(dayLi);
+    }
+    
+}
+}
+diasDoMês();
+
+                    // Exercício 2
