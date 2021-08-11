e7bde80ff9f2dd46ebb1192fc95e4a203413c365
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-11 16:28:32 -0300

    fixaçao 4

--- MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D03_Javascript_Eventos/Fixaçao/main.js
+++ MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D03_Javascript_Eventos/Fixaçao/main.js
@@ -2,7 +2,7 @@ const firstLi = document.getElementById('first-li');
 const secondLi = document.getElementById('second-li');
 const thirdLi = document.getElementById('third-li');
 const input = document.getElementById('input');
-const myWebpage = document.getElementById('my-spotrybefy');
+const myWebpage = document.getElementById('mySpotrybefy');
 
 /*
  Copie esse arquivo e edite apenas ele;
@@ -34,13 +34,23 @@ redirecione para alguma página;
 1. Que tal redirecionar para seu portifólio?*/
 
 function changePg (eventCP) {
-    window.location.replace('')
+    window.location.replace('https://jotapedrok.github.io/');
 }
+
+myWebpage.addEventListener('dblclick', changePg);
 
 /*
  Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
+*/
 
+function changeColor (eventCC) {
+    myWebpage.style.color = 'red';
+};
+
+myWebpage.addEventListener(over)
+
+/*
 Segue abaixo um exemplo do uso de event.target:
 */
 
