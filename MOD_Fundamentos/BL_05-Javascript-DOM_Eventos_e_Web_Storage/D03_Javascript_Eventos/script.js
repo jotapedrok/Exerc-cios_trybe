24581cad9fd3958da2d0731e0457428dda9b82a8
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-13 04:25:10 -0300

    exercicio 7

--- MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D03_Javascript_Eventos/script.js
+++ MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D03_Javascript_Eventos/script.js
@@ -128,24 +128,13 @@ function textZoomOut (event) {
 
 days.addEventListener('mouseout', textZoomOut);
 
+                    //Exercicio 7
+function newTask (NameTask) {
+    let task = document.createElement('span');
+    task.innerText = NameTask;
 
-// function dayMouseOver() {
-//     let days = document.querySelector('#days');
-  
-//     days.addEventListener('mouseover', function(event) {
-//       event.target.style.fontSize = '30px';
-//       event.target.style.fontWeight = '600';
-//     })
-//   };
-  
-//   function dayMouseOut() {
-//     let days = document.querySelector('#days');
-  
-//     days.addEventListener('mouseout', function(event) {
-//       event.target.style.fontWeight = '200';
-//       event.target.style.fontSize = '20px';
-//     })
-//   };
-  
-//   dayMouseOver();
-//   dayMouseOut();
+    let taskList = document.querySelector('.my-tasks');
+    taskList.appendChild(task);
+}
+
+newTask('Codar');
