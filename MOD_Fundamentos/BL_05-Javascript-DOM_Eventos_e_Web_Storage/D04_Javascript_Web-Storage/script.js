074ccad24c12362d5361ce2c9de03e4d3d84d2ec
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-12 21:12:54 -0300

    incio exercicios

--- MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D04_Javascript_Web-Storage/script.js
+++ MOD_Fundamentos/BL_05-Javascript-DOM_Eventos_e_Web_Storage/D04_Javascript_Web-Storage/script.js
@@ -1,5 +1,10 @@
-let sec = document.querySelector('#test');
-let body = document.querySelector('body');
-let pText = document.createElement('p');
-pText = body.innerText;
-document.appendChild(sec);
+function sizePossibles(){
+    let possibleFontSizes = [8, 9, 10, 11, 12, 14, 18, 24, 30, 36, 48, 60, 72, 96];
+    let sizeList = document.getElementById('fontsize');
+    for(let i = 0; i < possibleFontSizes.length; i += 1) {
+        let size = document.createElement('option');
+        size.innerText = possibleFontSizes[i];
+        sizeList.appendChild(size)
+    }
+}
+sizePossibles()
