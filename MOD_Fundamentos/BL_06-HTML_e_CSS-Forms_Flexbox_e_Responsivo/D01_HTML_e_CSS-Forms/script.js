7604884e12ee07cd70b6e3dda3297642cdfcfc1e
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-19 02:06:53 -0300

    proximo da desistencia

--- MOD_Fundamentos/BL_06-HTML_e_CSS-Forms_Flexbox_e_Responsivo/D01_HTML_e_CSS-Forms/script.js
+++ MOD_Fundamentos/BL_06-HTML_e_CSS-Forms_Flexbox_e_Responsivo/D01_HTML_e_CSS-Forms/script.js
@@ -12,19 +12,33 @@ addStates();
 
 function validateTxt(inputName, maxLength) {
   if (inputName.value === '' || inputName.value.length > maxLength) {
-    alert('Campo ' + inputName.name + ' inválido');
+    console.log('Campo "' + inputName.name + '" inválido');
+    errors.push('X');
   }
 }
+
 
 const namers = document.querySelector('#name');
 const email = document.querySelector('#email');
 const cpf = document.querySelector('#cpf');
 const endereço = document.querySelector('#endereço');
-const cidade = document.querySelector('#cidade')
+const cidade = document.querySelector('#cidade');
 const btnSend = document.querySelector('#submit');
 const resumVitae = document.querySelector('#vitae-resum');
 const cargo = document.querySelector('#cargo');
 const cargoDescri = document.querySelector('#cargo-descriçao');
+const datai = document.querySelector('#inicio-data');
+const estado = document.querySelector('#estado');
+const casa = document.querySelector('#casa');
+const ap = document.querySelector('#ap');
+function moradia() {
+  if (casa.checked === true) {
+    return "Casa"
+  } else if (ap.checked === true) {
+    return "Apartamento"
+  }
+}
+let errors = []
 
 function afterClick(event) {
   event.preventDefault();
@@ -38,5 +52,41 @@ function afterClick(event) {
   validateTxt(cargo, 40);
   validateTxt(cargoDescri, 500);
 
+  const data = document.querySelector('#inicio-data').value;
+  const day = data[0] + data[1];
+  const month = data[3] + data[4];
+  const year = data[6] + data[7] + data[8] + data[9];
+  function validateData() {
+    if (data.length === 10 && data[2] === '/' && data[5] === '/') {
+      if (year.length < 4) {
+        console.log('Formato de ano incorreto');
+        errors.push('X');
+      } else if (parseInt(day) < 0 || parseInt(day) > 31) {
+        console.log('Dia Inválido');
+        errors.push('X');
+      } else if (parseInt(month) < 0 || parseInt(month) > 12) {
+        console.log('Mês Inválido');
+        errors.push('X');
+      }
+    } else {
+      console.log('Campo "Data de início" inválido');
+      errors.push('X');
+    }
+  }
+  validateData();
+
+  function validateTypeHouse() {
+    if (casa.checked === false && ap.checked === false) {
+      console.log('Selecione um "Tipo de moradia');
+      errors.push('X');
+    }
+  }
+  validateTypeHouse();
+ if (errors.length === 0) {
+   const divA = document.createElement('div');
+   divA.className = 'containers';
+   divA.innerHTML = 
+   document.body.appendChild(divA);
+ } if 
 }
 btnSend.addEventListener('click', afterClick);
