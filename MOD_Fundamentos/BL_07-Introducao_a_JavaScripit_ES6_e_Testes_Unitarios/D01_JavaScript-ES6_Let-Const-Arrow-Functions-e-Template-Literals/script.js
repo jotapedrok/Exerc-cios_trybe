b763320c36a4c8f98d52a3becaec78ee49f2a064
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-27 17:24:55 -0300

    exercicio 2 - 4

--- MOD_Fundamentos/BL_07-Introducao_a_JavaScripit_ES6_e_Testes_Unitarios/D01_JavaScript-ES6_Let-Const-Arrow-Functions-e-Template-Literals/script.js
+++ MOD_Fundamentos/BL_07-Introducao_a_JavaScripit_ES6_e_Testes_Unitarios/D01_JavaScript-ES6_Let-Const-Arrow-Functions-e-Template-Literals/script.js
@@ -1,3 +1,5 @@
+          // Parte 1 - Exercicio 1
+
 const testingScope = escopo => {
   if (escopo) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -8,7 +10,51 @@ const testingScope = escopo => {
     console.log(elseScope);
   }
 }
-//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-// }
 
 testingScope(true);
+
+          // Parte 1 - Exercicio 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const returnEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+  return oddsAndEvens
+}
+
+
+
+console.log(returnEvens());
+
+          // Parte 2 - Exercicio 1
+
+const fatorial = (n) => {
+  if (n === 0) {
+    return 1;
+  } else {
+    let result = n;
+    for (let i = n-1; i > 0; i -= 1) {
+      result = result * i;
+    }
+    return result;
+  }
+}
+
+console.log(fatorial(5));
+
+          // Parte 2 - Exercicio 2
+const biggerWord = string => {
+  const array = string.split(' ');
+  let str = array[0];
+  for (let i in array) {
+    str.length < array[i].length ? str = array[i] : '';
+  }
+  console.log(str);
+}
+
+biggerWord('seu cuca bebe água de hipopotamos');
