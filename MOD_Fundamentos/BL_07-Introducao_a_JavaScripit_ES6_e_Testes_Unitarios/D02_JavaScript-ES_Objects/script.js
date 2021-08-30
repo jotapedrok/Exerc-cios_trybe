428fd5d9e26809753838a602af261197cba785eb
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-30 19:47:11 -0300

    exercicios bonus

--- MOD_Fundamentos/BL_07-Introducao_a_JavaScripit_ES6_e_Testes_Unitarios/D02_JavaScript-ES_Objects/script.js
+++ MOD_Fundamentos/BL_07-Introducao_a_JavaScripit_ES6_e_Testes_Unitarios/D02_JavaScript-ES_Objects/script.js
@@ -40,6 +40,7 @@ newOrder.name = 'Luiz Silva';
 newOrder.payment.total = 50;
 
 
+//Exercicio 1
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
   const send = `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº ${order.address.number}, AP: ${order.address.apartment}`;
@@ -48,6 +49,7 @@ const customerInfo = (order) => {
 
 customerInfo(order);
 
+//Exercicio 2
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
   const send2 = `Olá ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)[0]}, ${Object.keys(order.order.pizza)[1]} e ${order.order.drinks.coke.type} é R$ ${order.payment.total},00`;
@@ -77,11 +79,13 @@ const lesson3 = {
   turno: 'noite',
 };
 
+//Exercicio 1
 const changeShift = (objeto, key, value) => {
   objeto[key] = value;
 }
 changeShift(lesson2, 'turno', 'manhã');
 
+//Exercicio 2
 const keyList = (objeto) => {
   for (let i = 0; i < Object.keys(objeto).length; i += 1){
     console.log(`${Object.keys(objeto)[i]}`);
@@ -90,9 +94,11 @@ const keyList = (objeto) => {
 }
 keyList(lesson3);
 
+//Exercicio 3
 const objectSize = (objeto) => Object.keys(objeto).length;
 console.log(objectSize(lesson3));
 
+//Exercicio 4
 const valueList = (objeto) => {
   for (let i = 0; i < Object.values(objeto).length; i += 1){
     console.log(`${Object.values(objeto)[i]}`);
@@ -101,9 +107,11 @@ const valueList = (objeto) => {
 }
 valueList(lesson3);
 
+//Exercicio 5
 const allLessons = (Object.assign({}, {lesson1, lesson2, lesson3}));
 console.log(allLessons);
 
+//Exercicio 6
 const allStudents = (objeto) => {
   let allStud = 0;
   const array = Object.keys(objeto);
@@ -116,10 +124,12 @@ const allStudents = (objeto) => {
 
 allStudents(allLessons);
 
+//Exercicio 7
 const getValueByNumber = (objeto, num) => console.log(Object.values(objeto)[num]);
 
 getValueByNumber(lesson2, 2);
 
+//Exercicio 8
 const verifyPair = (objeto, key, value) => {
   const arrayArrays = Object.entries(objeto);
   let istrue = false;
@@ -132,3 +142,35 @@ const verifyPair = (objeto, key, value) => {
 }
 
 console.log(verifyPair(lesson1, 'professor', 'Maria Clara'));
+
+//Exercícios Bônus
+
+//Exercício 1
+const countMathStudents = (objeto, matéria) => {
+  const arrayArrays = Object.entries(objeto);
+  let count = 0;
+  for (let i = 0; i < arrayArrays.length; i += 1) {
+    if (arrayArrays[i][1].materia === matéria) {
+      count += arrayArrays[i][1].numeroEstudantes;
+    }
+  }
+  console.log(count);
+}
+countMathStudents(allLessons, 'Matemática');
+
+//Exercício 2
+const createReport = (objeto, professor) => {
+  const report = {};
+  report.professor = professor;
+  report.aulas = []
+  report.estudantes = 0
+  const arrayArrays = Object.entries(objeto);
+  for (let i = 0; i < arrayArrays.length; i += 1)  {
+    if (arrayArrays[i][1].professor === professor) {
+      report.aulas.push(arrayArrays[i][1].materia);
+      report.estudantes += arrayArrays[i][1].numeroEstudantes;
+    }
+  }
+  console.log(report)
+}
+createReport(allLessons, 'Maria Clara');
