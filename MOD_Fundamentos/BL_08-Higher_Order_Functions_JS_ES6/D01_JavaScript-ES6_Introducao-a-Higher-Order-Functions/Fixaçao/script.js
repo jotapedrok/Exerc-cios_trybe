3c00b18d188420dcb429ef6fbcf189713483e4c3
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-09-02 15:53:55 -0300

    fixaçao

---
+++ MOD_Fundamentos/BL_08-Higher_Order_Functions_JS_ES6/D01_JavaScript-ES6_Introducao-a-Higher-Order-Functions/Fixaçao/script.js
@@ -0,0 +1,9 @@
+const doingThings = (func) => console.log(func());
+
+const wakeup = () => 'Acordando!!'
+const breakFest= () => 'Bora tomar café!!'
+const goToSleep = () => 'Partiu dormir!!'
+
+doingThings(wakeup);
+doingThings(breakFest);
+doingThings(goToSleep);
