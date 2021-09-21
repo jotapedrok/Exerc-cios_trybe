c82b1ba8c7e86ea12fa8d1e68efc6e3bcac24df7
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-09-21 19:13:49 -0300

    exercicio 5

--- MOD_Fundamentos/BL_10-Testes_automatizados_com_Jest/exercicios/__tests__/objectsCompare.test.js
+++ MOD_Fundamentos/BL_10-Testes_automatizados_com_Jest/exercicios/__tests__/objectsCompare.test.js
@@ -1,1 +1,10 @@
 const { obj1, obj2, obj3 } = require('../src/objectsCompare');
+
+describe('exercício 5 - 7.3', () => {
+  it('Comparando "obj1" e "obj2" e eles devem ser identicos', () => {
+    expect(obj1).toEqual(obj2);
+  });
+  it('Comparando "obj1" e "obj3" e eles devem ser diferentes', () => {
+    expect(obj1).not.toEqual(obj3);
+  });
+});
