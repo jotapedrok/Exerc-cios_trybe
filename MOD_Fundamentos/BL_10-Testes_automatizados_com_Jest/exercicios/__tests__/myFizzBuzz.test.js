17d495405d3055d6ebc31bd6eb4e740002cbbc25
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-09-21 19:06:55 -0300

    exercicio 4

--- MOD_Fundamentos/BL_10-Testes_automatizados_com_Jest/exercicios/__tests__/myFizzBuzz.test.js
+++ MOD_Fundamentos/BL_10-Testes_automatizados_com_Jest/exercicios/__tests__/myFizzBuzz.test.js
@@ -1,1 +1,19 @@
 const myFizzBuzz = require('../src/myFizzBuzz');
+
+describe('exercício 4 - 7.3', () => {
+  it('A função recebe como parâmetro o número 15 e retorna "fizzbuzz"', () => {
+    expect(myFizzBuzz(15)).toBe("fizzbuzz");
+  });
+  it('A função recebe como parâmetro o número 9 e retorna "fizz"', () => {
+    expect(myFizzBuzz(9)).toBe("fizz");
+  });
+  it('A função recebe como parâmetro o número 10 e retorna "buzz"', () => {
+    expect(myFizzBuzz(10)).toBe("buzz");
+  });
+  it('A função recebe como parâmetro o número 8 e retorna 8', () => {
+    expect(myFizzBuzz(8)).toBe(8);
+  });
+  it('A função não recebe um "number" como parâmetro e retorna "false"', () => {
+    expect(myFizzBuzz("5")).toBe(false);
+  });
+});
