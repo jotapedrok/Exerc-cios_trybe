05bd7c3124995a1f10369de9b9d37b96ab970f4e
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-09-22 22:55:42 -0300

    exercicios 2 e 3

--- MOD_Fundamentos/BL_10-Testes_automatizados_com_Jest/D02/__tests__/exercicios.test.js
+++ MOD_Fundamentos/BL_10-Testes_automatizados_com_Jest/D02/__tests__/exercicios.test.js
@@ -4,7 +4,6 @@ const {
 } = require('../src/exercicios');
 
 describe('Testes da função uppercase', () => {
-  
   it('Testa se o retorno da função é a string passada como parâmetro em letras maiúsculas', (done) => {
     const callback = (data) => {
       try {
@@ -15,5 +14,19 @@ describe('Testes da função uppercase', () => {
       }
     }
     uppercase('banana', callback)
+  });
+});
+
+describe('Testes da função "getUserName"', () => {
+  it('Deve retornar o nome do funcionário referênte ao ID passado como parâmetro', async () => {
+   expect(await getUserName(1)).toBe('Mark');
+  });
+  it('Deve retornar um erro informando que não existe um funcionário com o ID referido', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(4);
+    } catch (e) {
+      expect(e.message).toBe('User with 4 not found.');
+    }
   });
 });
