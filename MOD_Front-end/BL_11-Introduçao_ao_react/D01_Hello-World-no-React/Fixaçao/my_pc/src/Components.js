1a8d0b3414938490773e597c67c6e747582302c1
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-10-04 19:04:53 -0300

    cria arquivos react

---
+++ MOD_Front-end/BL_11-Introduçao_ao_react/D01_Hello-World-no-React/Fixaçao/my_pc/src/Components.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Components extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>JOÃO PEDRO DE AGUIAR SANTOS</h1>
+        <p>Jovem, Feliz, Preguiça de escrever mais coisa, então por hoje é só</p>
+      </div>
+    )
+  }
+}
+
+export default Components;
