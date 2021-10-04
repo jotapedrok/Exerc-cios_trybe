7aba4babab0481b33d239dad9d4d565942acd718
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-10-04 19:16:09 -0300

    exercicios 1 e 2

--- MOD_Front-end/BL_11-Introduçao_ao_react/D01_Hello-World-no-React/task_list/src/App.js
+++ MOD_Front-end/BL_11-Introduçao_ao_react/D01_Hello-World-no-React/task_list/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+const tasks = ['comer', 'dormir', 'jogar bola', 'codar', 'sei lá'];
+const Task = (array) => {
+  return (
+    array.map((task) => {
+      return (<li>{task}</li>)
+    })
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ol>
+        {Task(tasks)}
+      </ol>
     </div>
   );
 }
