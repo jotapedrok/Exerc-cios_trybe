262856c4784205f4856133936cb0a40a67666c36
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-10-06 00:45:37 -0300

    remove fundamento

--- MOD_Fundamentos/BL_08-Higher_Order_Functions_JS_ES6/D01_JavaScript-ES6_Introducao-a-Higher-Order-Functions/Fixaçao/script.js
+++
@@ -1,9 +0,0 @@
-const doingThings = (func) => console.log(func());
-
-const wakeup = () => 'Acordando!!'
-const breakFest= () => 'Bora tomar café!!'
-const goToSleep = () => 'Partiu dormir!!'
-
-doingThings(wakeup);
-doingThings(breakFest);
-doingThings(goToSleep);
