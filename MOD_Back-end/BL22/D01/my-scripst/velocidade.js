ad9314e6ff0ed9d21c33afa8e52cfaf01d010794
jotapedrok <jotapedrok@gmail.com>
2022-02-09 11:28:40 -0300

    scripts imc and velocidade

---
+++ MOD_Back-end/BL22/D01/my-scripst/velocidade.js
@@ -0,0 +1,8 @@
+const readline = require('readline-sync');
+
+const distancia = readline.questionInt('Qual a distância?(em metros)');
+const tempo = readline.questionInt('Quanto tempo será necessário para percorrer essa distância?(em segundos)');
+
+const vm = (distancia / tempo);
+
+console.log(`A velocidade média, com esses dados, é de ${vm} m/s`)
